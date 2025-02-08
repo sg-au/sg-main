f3a0b789e7a996f4017a92cf3761763c260b3cb5
samyakbaid <Samyakbaid11@gmail.com>
2025-02-09 00:33:22 +0530

    makor changes done

--- src/public/platform/assets/js/darkmode.js
+++ src/public/platform/assets/js/darkmode.js
@@ -1,23 +1,23 @@
-function darkMode() {
-    var darkBtn = document.getElementById("darkbtn");
-    var lightBtn = document.getElementById("lightbtn");
+// function darkMode() {
+//     var darkBtn = document.getElementById("darkbtn");
+//     var lightBtn = document.getElementById("lightbtn");
 
-    // Toggle visibility of buttons
-    darkBtn.style.display = "none";
-    lightBtn.style.display = "block";
+//     // Toggle visibility of buttons
+//     darkBtn.style.display = "none";
+//     lightBtn.style.display = "block";
 
-    // Apply dark mode theme
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-}
+//     // Apply dark mode theme
+//     document.documentElement.setAttribute("data-bs-theme", "dark");
+// }
 
-function lightMode() {
-    var darkBtn = document.getElementById("darkbtn");
-    var lightBtn = document.getElementById("lightbtn");
+// function lightMode() {
+//     var darkBtn = document.getElementById("darkbtn");
+//     var lightBtn = document.getElementById("lightbtn");
 
-    // Toggle visibility of buttons
-    darkBtn.style.display = "block";
-    lightBtn.style.display = "none";
+//     // Toggle visibility of buttons
+//     darkBtn.style.display = "block";
+//     lightBtn.style.display = "none";
 
-    // Revert to light mode theme
-    document.documentElement.setAttribute("data-bs-theme", "light");
-}
+//     // Revert to light mode theme
+//     document.documentElement.setAttribute("data-bs-theme", "light");
+// }
